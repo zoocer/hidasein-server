b3d211a5627cc52f1f84de104466f769e243d20f
Wang Shudao <shudao.wang@me.com>
2019-05-19 22:12:58 +0800

    add: page query

--- routes/talkApi.js
+++ routes/talkApi.js
@@ -2,16 +2,27 @@ const _ = require('lodash')
 const Talk = require('../models/Talk')
 const randomIpv4 = require('random-ipv4')
 const geoip = require('geoip-country')
+const Redis = require('ioredis')
+const moment = require('moment')
+
+const redis = new Redis(6379)
 
 exports.getTalks = async (req, res, next) => {
   try {
-    Talk.find({}, null, { sort: { date: -1 } }, function(err, talks) {
-      if (err) {
-        next(err)
-        return
+    const page = req.query.page || 0
+    const pageSize = 5
+    Talk.find(
+      {},
+      null,
+      { skip: pageSize * page, limit: pageSize, sort: { date: -1 } },
+      function(err, talks) {
+        if (err) {
+          next(err)
+          return
+        }
+        res.json(talks)
       }
-      res.json(talks)
-    })
+    )
   } catch (e) {
     next(e)
   }
@@ -21,6 +32,18 @@ exports.createTalk = async (req, res, next) => {
   try {
     // let ip = req.clientIp
     let ip = randomIpv4()
+    let lastDate = await redis.get(ip)
+    console.log(`last date for ${ip}: `, lastDate)
+    let diff = moment().diff(lastDate, 'days')
+    console.log('diff days: ', diff)
+    if (diff < 1) {
+      res.json({
+        code: 400,
+        data: { message: 'Only one submition is allowed in 24 hours' }
+      })
+      return
+    }
+    await redis.set(ip, new Date())
     let geo = geoip.lookup(ip)
     let code = ''
     if (geo) {
