4a9a9b52d334a720bcb70bbcea765a11fba4aa9c
yifei <flylee.bjfu@gmail.com>
2019-06-20 10:31:41 +0800

    build;

---
+++ public/precache-manifest.1f09f0b7f86aeea5c06eb14e2cc3e1e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca02e21b415b41d91f86",
+    "url": "/static/js/main.6ee566bf.chunk.js"
+  },
+  {
+    "revision": "2ed91646200d44101dbb",
+    "url": "/static/js/2.9efd1d8f.chunk.js"
+  },
+  {
+    "revision": "ca02e21b415b41d91f86",
+    "url": "/static/css/main.0cd898a6.chunk.css"
+  },
+  {
+    "revision": "e6ddf1ad4d708200a01fcaf0b9ce56a7",
+    "url": "/index.html"
+  }
+];
